5a765997502d32dfc8b2f0af3f16d35f9cca61e0
Lucas Laibly <original7245@gmail.com>
2021-10-18 13:20:24 -0700

    unary grpc service

---
+++ README.md
@@ -0,0 +1,21 @@
+### Get protoc installed. Follow google instructions.
+### ... also brew works as well
+
+## To Run Server
+- `go run usermgmt_server/usermgmt_server.go`
+- `enter`
+
+~~~ open another terminal ~~
+
+## To Run Client
+- `go run usermgmt_client/usermgmt_client.go`
+- `enter` 
+
+## What to expect
+Server Terminal will dump out "Received: {user-information}" with a time stamp
+Client Terminal will dump out "User Details: NAME {name} AGE {age} ID {id}"
+
+This is a unary service, which means that the server is waiting for the inbound message to come from the client. 
+It seems to be essentially the same thing as an SDK, or API request, or something of that nature.. 
+
+There are (3) count em THREE other types of services that can be made with gRPC (Server Stream, Client Stream, and Bi-Stream). Someday I will learn those as well.
---
+++ usermgmt_server/usermgmt_server.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"log"
+	"math/rand"
+	"net"
+
+	pb "example.com/go-usermgmt-grpc/usermgmt"
+	"google.golang.org/grpc"
+)
+
+// set port system is to run on
+const (
+	port = ":50051"
+)
+
+type UserManagementServer struct {
+	pb.UnimplementedUserManagementServer
+}
+
+func (s *UserManagementServer) CreateNewUser(ctx context.Context, in *pb.NewUser) (*pb.User, error) {
+	// get the name of the new user being created
+	log.Printf("Received: %v", in.GetName())
+
+	// generate random user id (not actually random)
+	var user_id int32 = int32(rand.Intn(1000))
+
+	// return User
+	return &pb.User{
+		Name: in.GetName(),
+		Age:  in.GetAge(),
+		Id:   user_id,
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+
+	if err != nil {
+		log.Fatal("Failed to listen: %v", err)
+	}
+
+	// Make a new server
+	s := grpc.NewServer()
+
+	// Register server as new service
+	pb.RegisterUserManagementServer(s, &UserManagementServer{})
+	log.Printf("Server Listening at %v", lis.Addr())
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatal("Failed to server: %v", err)
+	}
+}
